e5d26ceefa06e084de9833b913b55536487e7701
Vaclav Rozsypalek <vaclav@giantswarm.io>
2017-10-20 16:26:41 +0200

    final config

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -146,7 +146,7 @@ exec $TASKSET /usr/bin/qemu-system-x86_64 \
   -m ${MEMORY} \
   -enable-kvm \
   -device virtio-net-pci,netdev=${NETWORK_TAP_NAME} \
-  -netdev tap,id=${NETWORK_TAP_NAME},br=${NETWORK_BRIDGE_NAME},ifname=${NETWORK_TAP_NAME} \
+  -netdev tap,id=${NETWORK_TAP_NAME},br=${NETWORK_BRIDGE_NAME},ifname=${NETWORK_TAP_NAME},downscript=no \
   -fsdev \
   local,id=conf,security_model=none,readonly,path=/usr/code/cloudconfig \
   -device virtio-9p-pci,fsdev=conf,mount_tag=config-2 \
