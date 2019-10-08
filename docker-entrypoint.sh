5bff71bfa75ac720f4752792fe2f73d1b1b0b96d
Vaclav Rozsypalek <vaclav@giantswarm.io>
2019-10-09 00:24:55 +0200

    format qemu node setup

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -141,11 +141,15 @@ sleep 10s
 #        Colon separated list of NTP servers.
 #  -out string
 #        Path to save resulting ignition config.
-/qemu-node-setup -node-ip=${MY_POD_IP} -dns-servers=${DNS_SERVERS} -hostname=${HOSTNAME} -main-config="${raw_ignition_dir}/${ROLE}.json" \
-                 -ntp-servers=${NTP_SERVERS} -out="${raw_ignition_dir}/final.json"
+/qemu-node-setup -node-ip="${MY_POD_IP}" \
+		-dns-servers="${DNS_SERVERS}" \
+		-hostname="${HOSTNAME}" \
+		-main-config="${raw_ignition_dir}/${ROLE}.json" \
+		-ntp-servers="${NTP_SERVERS}" \
+		-out="${raw_ignition_dir}/final.json"
 
 # rewrite eth packet destination MAC address from container eth0 to the VM eth0 via tc
-tc qdisc del dev eth0 || echo "tc qdisc clear"
+tc qdisc del dev eth0 handle ffff: || echo "tc qdisc clear"
 tc qdisc add dev eth0 handle ffff: ingress
 tc filter add dev eth0 parent ffff: protocol all u32 match u32 0 0 action pedit ex munge eth dst set ${MAC_ADDRESS}
 
