39b1734883d93e097b11a8d0f04faafb363740d7
Marcel Mueller <marcel.mueller1@rwth-aachen.de>
2020-07-13 13:09:16 +0200

    Add file output

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -181,6 +181,7 @@ eval exec "$TASKSET" /usr/bin/qemu-system-x86_64 \
   -m "$MEMORY" \
   -enable-kvm \
   -d guest_errors,unimp,cpu_reset,exec,cpu \
+  -D "/tmp/qemu.log" \
   -device virtio-net-pci,netdev="$NETWORK_TAP_NAME",mac="$MAC_ADDRESS" \
   -netdev tap,id="$NETWORK_TAP_NAME",ifname="$NETWORK_TAP_NAME",downscript=no \
   -fw_cfg name=opt/org.flatcar-linux/config,file="$RAW_IGNITION_DIR"/final.json \
